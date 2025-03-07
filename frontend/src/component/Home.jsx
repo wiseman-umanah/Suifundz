9ee022d0ec6a6683af03a5e22967e0be1386af73
Immanueljnr <elijaheffiong088@gmail.com>
2025-03-07 15:51:17 +0100

    wallet setup but not complete

--- frontend/src/component/Home.jsx
+++ frontend/src/component/Home.jsx
@@ -1,124 +1,20 @@
-import {
-  AppBar,
-  Box,
-  Button,
-  Container,
-  Toolbar,
-  Typography,
-  IconButton,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
-import React, { useState } from "react";
-import MenuIcon from "@mui/icons-material/Menu";
-import Image from "../assets/Cards.svg";
-
-const Home = () => {
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+const AppWithWallet = () => {
   return (
-    <Box
-      sx={{
-        // width: "8%",
-        height: "100vh",
-
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
+    <WalletProvider
+      autoConnect={false}
+      adapters={[
+        new SuiWalletAdapter(),
+        new SuietWalletAdapter(),
+        new EthosWalletAdapter(),
+      ]}
+      onConnectError={(error) => {
+        console.error("Wallet connection error:", error);
+        alert(`Wallet connection error: ${error.message}`);
       }}
     >
-      <AppBar
-        position="static"
-        sx={{
-          backgroundColor: "transparent", // Removes background
-          boxShadow: "none", // Removes shadow
-          width: "90%",
-          // border: "1px solid red",
-          padding: 0,
-        }}
-      >
-        <Toolbar>
-          <Typography
-            variant="h6"
-            color="#000000"
-            sx={{ flexGrow: 1, fontWeight: "bold", padding: 0, margin: 0 }}
-          >
-            Suifundz
-          </Typography>
-
-          {/* Desktop Links */}
-          <Box sx={{ display: { xs: "none", md: "block" } }}>
-            <Button
-              variant="contained"
-              color="primary"
-              backgroundColor="#000000"
-              size="large"
-              sx={{ backgroundColor: "#000000", borderRadius: "50px" }}
-            >
-              Get Started
-            </Button>
-          </Box>
-
-          {/* Mobile Menu Button */}
-          <IconButton
-            color="inherit"
-            edge="end"
-            sx={{ display: { md: "none" } }}
-            onClick={handleDrawerToggle}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          flexGrow: 1,
-          width: "90%",
-          padding: "20px",
-
-          // border: "1px solid red",
-        }}
-      >
-        <Box sx={{ width: "40%" }}>
-          <Typography
-            variant="h2"
-            fontWeight="bold"
-            gutterBottom
-            color="#000000"
-            position="relative"
-          >
-            Connecting You To The Future Of Finance
-          </Typography>
-
-          <Button
-            variant="contained"
-            color="primary"
-            backgroundColor="#000000"
-            size="large"
-            sx={{
-              backgroundColor: "#000000",
-              borderRadius: "50px",
-              marginTop: "15px",
-            }}
-          >
-            Get Started
-          </Button>
-        </Box>
-        <Box sx={{ width: "40%" }}>
-          <img src={Image} alt="img" className="w-full" />
-        </Box>
-      </Box>
-    </Box>
+      <App />
+    </WalletProvider>
   );
 };
 
-export default Home;
+export default AppWithWallet;
